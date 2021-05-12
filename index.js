dd6c8b4c241e2ca266a587e5ec72b0b5bc452d29
Yrmee <80338690+Yrmee@users.noreply.github.com>
2021-05-12 11:26:45 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const makePizza = function () {
+    console.log("1. Make the dough and let rest.");
+    console.log("2. Cut the ingredients for topping.");
+    console.log("3. Make the sauce as says on the label.");
+    console.log("4. Knead the dough and spread out like a pizza.");
+    console.log("5. Put on sauce, add some cheese and toppings.");
+    console.log("6. Bake your pizza in the oven.");
+    console.log("7. Enjoy your pizza! Be carefull, it might be hot.");
+};
+
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log("1. Boil rise.");
+    console.log("2. Cut the ingredients.");
+    console.log("3. Drain rise, mix it with rise vinegar and let rest.");
+    console.log("4. Wet your fingers and get a Nori-sheet.");
+    console.log("5. Put rise on your Nori-sheet, and add your ingredients.");
+    console.log("6. Roll it into a sushi roll and cut it into equal pieces.");
+    console.log("7. Enjoy your sushi. Try it with soy sauce!");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
